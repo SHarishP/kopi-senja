1357ab2731a5d14f6a3d70897bba0496f72196df
Setiawan Harish <setiawanharish85@gmail.com>
2023-07-23 14:16:46 +0700

    Menambahkan Search Bar

--- script.js
+++ script.js
@@ -2,16 +2,37 @@
 const navbarNav = document.querySelector(".navbar-nav");
 const menu = document.querySelector("#menu");
 
-// Ketika Hamburger Menu di klik
-
-function toggleMenu() {
+// Fungsi untuk menambahkan class active pada hamburger menu
+function toggleMenu(e) {
   navbarNav.classList.toggle("active");
+  e.preventDefault();
 }
 
+//Untuk membuat hamburger menu dapat menambahkan class active
 menu.addEventListener("click", toggleMenu);
 
+//Untuk menghilangkan list menu
 document.addEventListener("click", function (e) {
   if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+});
+
+// Variable Class Active pada Search Bar
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+const searchBtn = document.querySelector("#search-btn");
+
+//Fungsi untuk menambahkan class Active pada Search-Form
+searchBtn.onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// Untuk menghilangkan Search Form
+document.addEventListener("click", function (e) {
+  if (!searchBtn.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
